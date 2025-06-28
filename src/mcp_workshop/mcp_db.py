e0830c82d78ed10fb0ca996eab93c1e42a118e73
wyhwong <wyhwong@link.cuhk.edu.hk>
2025-06-28 11:00:50 +0800

    Answer for mcp_db.py
    Typo

--- src/mcp_workshop/mcp_db.py
+++ src/mcp_workshop/mcp_db.py
@@ -33,3 +33,44 @@ Call get_photo_description_from_database(filepath) to get a photo description by
 #     and implement the mentioned functions.                                  #
 # - You may refer to mcp_fs.py as an example to write a MCP server.           #
 ###############################################################################
+
+mcp = FastMCP(
+    name="DatabaseAssistant",
+    instructions=INSTRUCTIONS,
+    on_duplicate_tools="error",
+)
+
+
+@mcp.tool()
+def insert_photo_description_to_database(filepath: str, description: str) -> str:
+    """Insert a photo description into the database."""
+
+    status = insert_photo_description_record(SESSION_FACTORY, filepath, description)
+
+    if status is True:
+        return "Photo description inserted successfully."
+
+    return "Failed to insert photo description for unknown reason."
+
+
+@mcp.tool()
+def list_photo_descriptions_in_database() -> str:
+    """List all photo descriptions in the database."""
+
+    return str(load_all_photo_descriptions(SESSION_FACTORY))
+
+
+@mcp.tool()
+def get_photo_description_from_database(filepath: str) -> str:
+    """Get a photo description by its ID."""
+
+    description = get_photo_description(SESSION_FACTORY, filepath)
+
+    if description:
+        return description
+
+    return "No description found for the given file path."
+
+
+if __name__ == "__main__":
+    mcp.run(transport="sse", port=env.MCP_PG_PORT)
